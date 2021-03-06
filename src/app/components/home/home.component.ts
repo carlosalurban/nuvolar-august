be63ddbbe28cc073c1a1f291d125751b9c10f482
carlosalurban <carlosalvarezurbano@gmail.com>
2021-03-06 12:59:48 +0100

    log api response

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -22,6 +22,10 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.getUsersFromApi();
 
+    this.apiService.getUsers().subscribe((userApi: Array<Users>) => {
+      console.log(userApi);
+    });
+
   }
 
   /**
